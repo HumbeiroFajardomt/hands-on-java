45989eb3ca1a64ab9bd1b411327da39625f3ff72
HumbeiroFajardomt <113655610+HumbeiroFajardomt@users.noreply.github.com>
2022-12-28 15:14:29 +0000

    Accounts Get Data Method

--- src/main/java/bank/DataSource.java
+++ src/main/java/bank/DataSource.java
@@ -14,7 +14,7 @@ public class DataSource {
     Connection connection = null;
     //it is neccesary handle the exception for getConnection 
     
-    try{
+    try{ 
       connection = DriverManager.getConnection(db_file);
       System.out.println("we're Connected");//Message to check Connection 
     }catch(SQLException e){
@@ -30,14 +30,14 @@ public class DataSource {
 //to craft a sql query that  will get the customer data from the databases
 //Using a String  we put the variable
 String sql = "select * from customers where username = ?"; //it is not a good practice put the variable username,
-//it could be used  as a sql injection meant to stel data
+//it could be used  as a sql injection meants to stel data
 // put ? to serve as a placeholder // technique used to query the database will be safer
 Customer customer = null; // to get access to customer var
 //3 try catch with the resources to close the connection 
 try(Connection connection = connect(); 
 //additional resources
     PreparedStatement statement = connection.prepareStatement(sql)){
-      //set Values for placeholders set method to do so ? is string so cal string method
+      //set Values for placeholders set method to do so ? is string so call string method
       //possition placeholder ? = 1 there is just one and value = username
 statement.setString(1, username);
 //to execute query and add result set to complete the query and it is autoclosed
@@ -57,11 +57,48 @@ try(ResultSet resultset = statement.executeQuery()){
 }
 return customer;
   }
+/////////////////////////////////////// getting Accounts /////////////// 
+// 4 
+public static Accounts getAccounts(int accountId){
+String sql = "select * from accounts where id = ?";
+
+Accounts account = null;
+try (Connection connection = connect(); 
+    PreparedStatement statement = connection.prepareStatement(sql)
+) {
+  statement.setInt(1, accountId);
+
+  try(ResultSet resultSet = statement.executeQuery()){
+    account = new Accounts(
+      resultSet.getInt("id"),
+      resultSet.getString("type"),
+      resultSet.getInt("balance")
+    );
+  }
+
+  
+} catch (SQLException e) {
+  e.printStackTrace();
+  // TODO: handle exception
+}
+
+return account;
+
+
+}
+
+
+///////////////////////////  Main //////////////////////////////////////////
 
 
   public static void main(String[] args){
    Customer customer =getCustomer("bdowsh4@mozilla.com");
-    System.out.println(customer.getPassword());
+    System.out.println(customer.getAccount_Id());
+    System.out.println(customer.getName());
+
+    Accounts accounts = getAccounts(customer.getAccount_Id());
+    System.out.println(accounts.getBalance());
+    System.out.println(accounts.getId());
   }
 
 }
